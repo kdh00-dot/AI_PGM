9f71ed9796ce0a3aaba5626e4f9b9b4ddedd2689
kdh00-dot <kdh3787@gmail.com>
2026-06-26 12:37:32 +0900

    Add files via upload

---
+++ 0626/stringEx.py
@@ -0,0 +1,23 @@
+print(" Hello Python")
+print(' Hello \nPy\nth\non')
+print(""" Hello 
+      Python""")
+a = "Life is too short, You need Python"
+print(a[12:17])
+print(a[28:35])
+
+
+# a = 1 (literal or constant)
+# a = b (variable)
+# a = 1+b (operator)
+# a = sum(1, 2) (function)
+a = 3
+b = 5
+print("I eat %d apples." % a)
+print("T eat %d apples and %f oranges." %((1+2), 3.141592))
+print("I eat %d apples and %.2f oranges." %((1+2), 3.141592))
+
+print("I eat {} apples".format(3))
+print("I eat {0} apples and {1} oranges.".format(3, 5))
+print(f"I eat 3 apples and 5 oranges.")
+print(f"I eat {3} apples and {5} oranges.")
